80539d6c1561001ee9f00ccf2db5a8fe61705878
Lemon Pecan Pie <aidanhpaul@gmail.com>
2025-08-30 11:52:33 -0400

    Add statement guessing born year

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ const Hello = (props) => {
       <p>
         Hello {props.name}, you are {props.age} years old
       </p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   )
 }
